1eda2ba3943edb4658c75605e16df34e5232f842
Martin Wagner <martin@novasa.com>
2021-12-01 15:22:41 +0100

    Theme nedarver nu fra XML Themes

--- styling/src/main/java/com/example/theme/Theme.kt
+++ styling/src/main/java/com/example/theme/Theme.kt
@@ -3,10 +3,12 @@ package com.example.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import com.google.android.material.composethemeadapter.createMdcTheme
 
 
 private val DarkColorPalette = darkColors(
@@ -34,10 +36,26 @@ fun FactsTheme(
         LightColorPalette
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
+    val context = LocalContext.current
+    val layoutDirection = LocalLayoutDirection.current
+    val (color, typography, shapes) = createMdcTheme(
+        context = context,
+        layoutDirection = layoutDirection
     )
+
+    if(color != null && typography != null && shapes != null){
+        MaterialTheme(
+            colors = color,
+            typography = typography,
+            shapes = shapes,
+            content = content
+        )
+    } else {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
